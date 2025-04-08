703a4b3381f3a7fc82740d0d7fc4432d79d8be88
whosatyamraj <satyamrajsingh0811@gmail.com>
2025-04-08 23:01:42 +0530

    initial commit

---
+++ spread.js
@@ -0,0 +1,4 @@
+let spread = ["50", "much", "fun"];
+let message = ["JavaScript", "is", ...spread, "and", "very", "powerful"];
+let newMessage = [...message, "to", "learn"];
+console.log(newMessage);
