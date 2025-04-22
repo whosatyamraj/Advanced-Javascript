cd1b75ca5da0fcd6a2495af953b24ae492a974c1
whosatyamraj <satyamrajsingh0811@gmail.com>
2025-04-22 13:09:37 +0530

    Constructor and its properties

--- classes.js
+++ classes.js
@@ -36,15 +36,93 @@ constructor(firstname,lastname){
 }
     */
 
-//METHODS
+// create a js function to display the loacal date and time using the date method 
 
-class Person {
-    constructor(firstname,lastname){
-        this.firstname=firstname;
-        this.lastname=lastname;
-    }
-    greet(){
-        console.log("hi there!",this.firstname);
+/*function displayDateTime(){
+    let date = new Date();
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    let formattedDate = date.toLocaleDateString('en-US', options);
+    console.log(formattedDate);
+}*/
+
+//METHODS
+// Function on an object is called a methods. When define the methods , we don't use the function keyword. we directly use the name of the method.
+
+// class Person {
+//     constructor(firstname,lastname){
+//         this.firstname=firstname;
+//         this.lastname=lastname;
+//     }
+//     greet(){
+//         console.log("hi there!",this.firstname);
         
-    }
-    }
+//     }
+//     compliment(){
+//         console.log("you are doing great",this.firstname);
+//     }
+// }
+
+// let compliment = new Person("Harry","Hat");
+// console.log(compliment);
+// //output: Person { firstname: 'Harry', lastname: 'Hat' }
+// greet.call(compliment);
+// //output: hi there! Harry
+
+
+//PROPERTIES
+// Properties are the values associated with an object. In JavaScript, an object is a standalone entity, with properties and type. It is similar to real-life objects, like a car, which has properties like weight and color, and methods like start and stop.
+
+// class Person {
+//     #firstname; // private property
+//     #lastname; // private property
+//     constructor(firstname,lastname){
+//         this.#firstname=firstname;
+//         this.#lastname=lastname;
+//     }
+//     let p = new Person("Marry","saga");
+//     console.log(p.#firstname); // output: Marry
+//     console.log(p.#lastname); // output: saga
+
+//     constructor(firstname,lastname){
+//         if(firstname.startswith("M")){
+//             this.#firstname=firstname;
+//         }else{
+//             this.#firstname="M"+firstname;      
+        
+//         }
+//         this.#lastname=lastname;
+//     }
+//     let p = new Person("Marry","saga");
+//     console.log(p.#firstname); // output: Marry
+//     console.log(p.#lastname); // output: saga
+
+
+
+// class Person {
+//     #firstname; // private property
+//     #lastname; // private property
+//     constructor(firstname,lastname){
+//         this.#firstname=firstname;
+//         this.#lastname=lastname;
+//     }
+//     get firstname(){
+//         return this.#firstname;
+//     }
+//     set firstname(val){
+//         this.#firstname=value;
+//     }
+//     get lastname(){
+//         return this.#lastname;
+//     }
+//     set lastname(value){
+//         this.#lastname=value;
+//     }
+// }   
+// let p = new Person("Marria","saga");
+// console.log(p.firstname); // output: Marria
+// console.log(p.lastname); // output: saga
+// p.firstname="Marrie";
+// console.log(p.firstname); // output: Marrie  
+// console.log(p.lastname); // output: saga
+// p.lastname="saga";
+
